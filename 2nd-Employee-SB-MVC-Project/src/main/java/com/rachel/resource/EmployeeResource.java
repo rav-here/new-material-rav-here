f95d6d3815f86477bfd0d3a25c5d6f0cc34bb3db
Rachel Vickerman <rachvick21@hotmail.co.uk>
2022-12-05 16:51:31 +0000

    December 5th, more resource class stuff

--- 2nd-Employee-SB-MVC-Project/src/main/java/com/rachel/resource/EmployeeResource.java
+++ 2nd-Employee-SB-MVC-Project/src/main/java/com/rachel/resource/EmployeeResource.java
@@ -1,12 +1,12 @@
 package com.rachel.resource;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +27,10 @@ public class EmployeeResource {
 		return employeeService.getAllEmployees();
 	}
 	// ============ resource for search employee by id ==================
+	// method: GET, POST, PUT, DELETE - request http methods! 
+	// produces: produces the object in the requested media type, specifies which media type(s) of representations a resource can give to the client
+	// consumes: consume the object with the requested media type, specifies which media type(s) of representations a resource can accept from the client
+
 	@RequestMapping(path = "/employees/{eid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Employee searchEmployeeByIdResource(@PathVariable("eid") int id) {
 		return employeeService.searchEmployeeById(id);
@@ -42,19 +46,37 @@ public class EmployeeResource {
 		return employeeService.employeeByDepartment(department);
 	}
 	// ============ resource for increment employee salary ==================
-	@RequestMapping(path = "/employees/increment/{eid}/{inc}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public boolean incrementEmployeeSalaryResource(@PathVariable("eid") int id, @PathVariable("inc") double inc) {
-		return employeeService.incrementSalary(id, inc);
+	@RequestMapping(path = "/employees/{eid}/{inc}", method = RequestMethod.PUT, produces = MediaType.TEXT_PLAIN_VALUE)
+	public String incrementEmployeeSalaryResource(@PathVariable("eid") int id, @PathVariable("inc") double inc) {
+		if (employeeService.incrementSalary(id, inc)) {
+			return "Salary Incremented";
+		}
+		else {
+			return "Salary NOT Incremented";
+		}
+		
 	}
-//	// ============ resource for increment employee salary ==================
-//	@RequestMapping(path = "/employees/add/{eid}/{name}/{designation}/{department}/{salary}/{doj}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-//	public boolean incrementEmployeeSalaryResource(@PathVariable("eid") int id, @PathVariable("name") String name, @PathVariable("designation") String desig, @PathVariable("department") String dept, @PathVariable("salary") double sal, @PathVariable("doj") LocalDate doj) {
-//		return employeeService.addEmployee(new Employee(id,name,desig,dept,sal,doj));
-//	}
-//	// ============ resource for delete employee by id ==================
-//	@RequestMapping(path = "/employees/increment/{eid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-//	public boolean deleteEmployeeByIdResource(@PathVariable("eid") int id) {
-//		return employeeService.deleteEmployee(id);
-//	}
+	// ============ resource for adding employee ==================
+	// path/URL is same as get method above but this is a post method so naming match doesn't matter
+	@RequestMapping(path = "/employees", method = RequestMethod.POST, produces = MediaType.TEXT_PLAIN_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String addEmployeeResource(@RequestBody Employee employee) {
+		 if (employeeService.addEmployee(employee)) {
+			 return "Record Added";
+		 }
+		 else {
+			 return "Record NOT Added";
+		 }
+		 
+	}
+	// ============ resource for delete employee by id ==================
+	@RequestMapping(path = "/employees/{eid}", method = RequestMethod.DELETE, produces = MediaType.TEXT_PLAIN_VALUE)
+	public String deleteEmployeeByIdResource(@PathVariable("eid") int id) {
+		if (employeeService.deleteEmployee(id)) {
+			return "Record Deleted";
+		}
+		else {
+			return "Record NOT Deleted";
+		}
+	}
 
 }
