2562e22c08774c0a3b0147496aabaaa428e1ad80
Rachel Vickerman <rachvick21@hotmail.co.uk>
2022-12-06 16:00:33 +0000

    Microservices Architecture Project complete

--- Share-Customer-REST-API-Consumer/src/main/java/com/rachel/controller/CustomerDetailController.java
+++ Share-Customer-REST-API-Consumer/src/main/java/com/rachel/controller/CustomerDetailController.java
@@ -24,18 +24,27 @@ public class CustomerDetailController {
 	
 	@RequestMapping("/checkCustomer") 
 	public ModelAndView checkCustomerController(@RequestParam("customerId") int customerId) {
-		// create empty MAV object
+		// create empty MAV 
 		ModelAndView modelAndView = new ModelAndView();
 		// get CustomerDetail object list via service layer object
 		List<CustomerDetail> customerDetails = customerDetailService.getCustomerDetailByCustomerId(customerId);
-		System.out.println(customerDetails);
-		// add the list to the MAV object
-		modelAndView.addObject("customerDetails", customerDetails);
-		// add the customerId field to MAV object
-		modelAndView.addObject("customerId", customerId);
-		// set the view that will display info on the web app
-		modelAndView.setViewName("ShowDetails");
-		// return MAV object
+		//System.out.println(customerDetails);
+		if (customerDetails.size() != 0) {
+			// add the list to the MAV 
+			modelAndView.addObject("customerDetails", customerDetails);
+			// add the customerId field to MAV 
+			modelAndView.addObject("customerId", customerId);
+			// set the view that will display info on the web app
+			modelAndView.setViewName("ShowDetails");
+		}
+		else {
+			// add message regarding error to MAV 
+			modelAndView.addObject("message", "Failed to load data, "+customerId+" is NOT a customer ID");
+			// set view to be same page, asking for customer Id
+			modelAndView.setViewName("InputCustomerId");
+		}
+		
+		// return MAV 
 		return modelAndView;
 	}
 
